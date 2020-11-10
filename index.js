b3b1a8f4afe869fbb4f1c0e7aa6945361cc32be3
Theodore Stansbury <tstansbu.27@gmail.com>
2020-11-10 00:32:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// put your own value below!
+const apiKey = 'Q9VGqgQjuju9g11v4UxmHZGfgc75qrZebDTiRKu5'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+    
+    console.log(responseJson);
+    $('#results-list').empty();
+    
+    for (let i = 0; i < responseJson.data.length; i++){
+     
+      $('#results-list').append(
+        `<li><h3>${responseJson.data[i].fullName}</h3>
+        <p>${responseJson.data[i].description}</p>
+        <a href="${responseJson.data[i].url}" target="_blank">Website</a>
+        </li>`
+      )};
+
+    
+    $('#results').removeClass('hidden');
+  };
+  
+  function getParkInfo(query, maxResults=10) {
+    const params = {
+      api_key: apiKey,
+      stateCode: [query],
+      limit: maxResults,
+    };
+
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
+  
+    console.log(url);
+  
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      });
+  }
+  
+  function watchForm() {
+    $('form').submit(event => {
+      event.preventDefault();
+      const searchTerm = $('#js-search-term').val();
+      const maxResults = $('#js-max-results').val();
+
+      $('#js-search-term').val('');
+      $('#js-max-results').val('');
+      getParkInfo(searchTerm, maxResults);
+    });
+  }
+  
+  $(watchForm);
